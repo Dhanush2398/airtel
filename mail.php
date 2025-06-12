0357597a73734c06dc0b7a3c9add2c7b35030289
dhanush <dhanush.dhoni007@gmail.com>
2025-06-12 12:43:30 +0530

    change

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST['name'] ?? '';
+$phone = $_POST['phone'] ?? '';
+$email = $_POST['email'] ?? '';
+$message = $_POST['message'] ?? '';
+
+$to = ""; 
+
+
+$subject = "Airtel Connection Request from $name";
+$body = "Name: $name\nPhone: $phone\nEmail: $email\nMessage:\n$message";
+$headers = "From: $email";
+
+
+if (!empty($name) && !empty($phone)) {
+    mail($to, $subject, $body, $headers);
+}
+
+
+header("Location: thankyou.html");
+exit;
+?>
